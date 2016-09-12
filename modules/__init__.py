333cc01f0d98320e67c3da1b2508d9d12335149f
Jan Max Meyer <jm@mausbrand.de>
2016-09-12 19:27:56 +0200

    Draft of a new automatic module importing script.

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,33 +6,35 @@
 #
 
 import os, logging
+import server.prototypes.basic
 
-viurModules = {}
+_viurModules = {}
 
-for viurModule in os.listdir(os.path.dirname(__file__)):
+for _module in os.listdir(os.path.dirname(__file__)):
 
-	if viurModule == "__init__.py" or not viurModule.endswith(".py"):
+	if _module == "__init__.py" or not _module.endswith(".py"):
 		continue
 
-	viurModule = viurModule[:-3]
+	_module = _module[:-3]
 
 	try:
-		_mod = __import__(viurModule, globals(), locals(), [viurModule])
-		viurModules[viurModule] = getattr(_mod, viurModule)
+		_import = __import__(_module, globals(), locals(), [_module])
 
-	except (ImportError, AttributeError):
-		try:
-			_mod = __import__(viurModule, globals(), locals(), [viurModule[0].upper() + viurModule[1:]])
-			viurModules[viurModule] = getattr(_mod, viurModule[0].upper() + viurModule[1:])
+		for _name in dir(_import):
+			if _name.startswith("_"):
+				continue
 
-		except (ImportError, AttributeError):
-			logging.error("Unable to import module %s" % viurModule)
+			_symbol = getattr(_import, _name)
+			if (getattr(_symbol, "__module__", None) != "modules.%s" % _module
+				or isinstance(_symbol, server.prototypes.basic.BasicApplication)):
+				continue
 
-		except:
-			raise
+			_viurModules[_name.lower()] = _symbol
+			logging.debug("Importing %s as %s" % (_symbol, _name.lower()))
 
 	except:
+		logging.error("Unable to import '%s'" % _module)
 		raise
 
-globals().update(viurModules)
-del viurModule, viurModules
+globals().update(_viurModules)
+del _viurModules, _module, _import, _name, _symbol
