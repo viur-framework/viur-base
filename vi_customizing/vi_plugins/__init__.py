4830bd69e5848b6c56f22555de1ef220b0d494a1
Jan Max Meyer <jm@mausbrand.de>
2017-11-22 16:04:35 +0100

    Added default files for Vi customizations.
    This is not necessary for setup.py generation.

---
+++ vi_customizing/vi_plugins/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# This is the major Vi plugin init file.
+# Do custom plugin imports and customized settings here.
+
+#import vi_plugins.bones
+
+# Change list settings
+#from config import conf
+#conf["batchSize"] = 64
+#conf["showBoneNames"] = False
+#conf["internalPreview"] = False
+
+# Change language
+# from i18n import setLanguage, getLanguage
+#conf["currentlanguage"] = "de"
+#setLanguage(conf["currentlanguage"])
+
+print(conf)
+print("!!! ALL PROJECT CUSTOMIZATIONS LOADED !!!")
