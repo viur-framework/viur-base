cae19557d473b99b03f1aea3811bf3d5a4a07c77
Tilman Oe <tilman.oe@gmail.com>
2017-11-23 17:16:25 +0100

    script

---
+++ clean-base.py
@@ -0,0 +1,45 @@
+import sys, os, subprocess, time, datetime
+
+whoami = raw_input("Enter Author Name: ")
+appid = raw_input("Enter desired app_id: ")
+time = time.time()
+timestamp = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+
+workdir = os.getcwd()+"/deploy"
+file_list = []
+replacements = {"{{app_id}}":appid, "{{whoami}}":whoami, "{{timestamp}}":timestamp}
+if os.path.exists(".git"):
+	print("Downloading submodules")
+	subprocess.check_output('git submodule init', shell=True)
+	subprocess.check_output('git submodule update', shell=True)
+	print("Removing .git tether")
+	try:
+		subprocess.check_output('git remote rm origin', shell=True)
+	except:
+		pass
+else:
+	print(".git tether already removed")
+
+for subdir, dirs, files in os.walk(workdir):
+	for file in files:
+		#print os.path.join(subdir, file)
+		filepath = subdir + os.sep + file
+
+		if filepath.endswith(".py") or filepath.endswith(".yaml"):
+			file_list.append(filepath)
+print (file_list)
+
+for file_obj in file_list:
+	lines = []
+	with open(file_obj) as infile:
+		for line in infile:
+			for src, target in replacements.iteritems():
+				line = line.replace(src, target)
+			lines.append(line)
+	with open(file_obj, 'w') as outfile:
+		for line in lines:
+			outfile.write(line)
+
+orig = os.path.join(workdir, "viur_server.py")
+newname = os.path.join(workdir, appid+".py")
+os.rename(orig, newname)
