d4e9d030f93b757fe64a9f1dedabb67e14e4cdd5
Sven Eberth <se@mausbrand.de>
2019-04-05 14:16:16 +0200

    improved gulp

--- sources/gulpfile.js
+++ sources/gulpfile.js
@@ -21,7 +21,7 @@ const del = require('del');
 const rename = require('gulp-rename');
 
 const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const zindex = require('postcss-zindex');
 const focus = require('postcss-focus');
@@ -34,86 +34,83 @@ const imagemin = require('gulp-imagemin');
 const cheerio = require('gulp-cheerio');
 
 // clean destination folder: remove css files
-gulp.task('clean', function () {
-	return del([
+gulp.task('clean', () =>
+	del([
 		destpaths.css + '/**/*',
-		destpaths.images+ '/**/*',
+		destpaths.images + '/**/*',
 		destpaths.icons + '/**/*',
-	], {force: true});
-});
+	], {force: true})
+);
 
 // compilation and postproduction of LESS to CSS
-gulp.task('css', function () {
-	var processors = [
-		nocomments, // discard comments
-		focus, // add focus to hover-states
-		zindex // reduce z-index values
-	];
-
-	return gulp.src('./less/style.less')
-	.pipe(less({
-		paths: [ path.join(__dirname, 'less', 'includes') ]
-	})) // compile less to css
-	.pipe(autoprefixer({
-		browsers: ['last 2 versions'],
-		cascade: false
-	})) // add vendor prefixes
-	.pipe(postcss(processors)) // clean up css
-	.pipe(jmq())
-	.pipe(stylefmt()) // syntax formatting
-	.pipe(rename('style.css'))
-	.pipe(gulp.dest(destpaths.css)) // save cleaned version
-	.pipe(nano()) // minify css
-	.pipe(rename('style.min.css'))
-	.pipe(gulp.dest(destpaths.css)); // save minified version
-});
+gulp.task('css', () =>
+	gulp.src('./less/style.less')
+		.pipe(less({
+			paths: [path.join(__dirname, 'less', 'includes')]
+		}))
+		.pipe(postcss([
+			autoprefixer({ // add vendor prefixes
+				browsers: ['last 2 versions'],
+				cascade: false
+			}),
+			nocomments, // discard comments
+			focus, // add focus to hover-states
+			zindex, // reduce z-index values
+		])) // clean up css
+		.pipe(jmq())
+		.pipe(stylefmt()) // syntax formatting
+		.pipe(gulp.dest(destpaths.css)) // save cleaned version
+		.pipe(nano()) // minify css
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(destpaths.css)) // save minified version
+);
 
 
 // reduce images for web
-gulp.task ('images', function () {
-	return gulp.src(srcpaths.images)
-    .pipe(imagemin([
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 5}),
-		imagemin.svgo({
-			plugins: [
-				{removeViewBox: false},
-				{removeDimensions: true}
-			]
-		})
-	]))
-	.pipe(gulp.dest(destpaths.images));
-});
+gulp.task('images', () =>
+	gulp.src(srcpaths.images)
+		.pipe(imagemin([
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: false},
+					{removeDimensions: true}
+				]
+			})
+		]))
+		.pipe(gulp.dest(destpaths.images))
+);
 
 // reduce icons for web
-gulp.task ('icons', function () {
-	return gulp.src(srcpaths.icons)
-    .pipe(imagemin([
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 5}),
-		imagemin.svgo({
-			plugins: [
-				{removeViewBox: false},
-				{removeDimensions: true}
-			]
-		})
-	]))
-    .pipe(cheerio({
-		run: function ($, file) {
-			$('style').remove()
-			$('[id]').removeAttr('id')
-			//$('[class]').removeAttr('class')
-			$('[fill]').removeAttr('fill')
-			$('svg').addClass('icon')
-		},
-		parserOptions: {xmlMode: true}
-    }))
-	.pipe(rename({prefix: "icon-"}))
-	.pipe(gulp.dest(destpaths.icons));
-});
+gulp.task('icons', () =>
+	gulp.src(srcpaths.icons)
+		.pipe(imagemin([
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: false},
+					{removeDimensions: true}
+				]
+			})
+		]))
+		.pipe(cheerio({
+			run: function ($, file) {
+				$('style').remove()
+				$('[id]').removeAttr('id')
+				//$('[class]').removeAttr('class')
+				$('[fill]').removeAttr('fill')
+				$('svg').addClass('icon')
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(rename({prefix: 'icon-'}))
+		.pipe(gulp.dest(destpaths.icons))
+);
 
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
 	gulp.watch(srcpaths.less, gulp.series('css'));
 	gulp.watch(srcpaths.icons, gulp.series('icons'));
 	gulp.watch(srcpaths.images, gulp.series('images'));
