8807ead56676a24d7ceeee1335d41468f8ebba23
Sven Eberth <se@mausbrand.de>
2019-12-17 17:31:36 +0100

    clean-base python3 only

--- clean-base.py
+++ clean-base.py
@@ -1,10 +1,15 @@
-#!/bin/sh
-# using the following stackoverflow answer to determine where /usr/bin/python points to
-# https://stackoverflow.com/questions/18993438/shebang-env-preferred-python-version
-''''which python2 >/dev/null 2>&1 && exec python2 "$0" "$@" # '''
-''''which python  >/dev/null 2>&1 && exec python  "$0" "$@" # '''
-''''exec echo "Error: I can't find python anywhere"         # '''
-import sys, os, subprocess, time, datetime, getpass, argparse, urllib, zipfile
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import datetime
+import getpass
+import os
+import subprocess
+import sys
+import time
+import urllib.request
+import zipfile
 
 try:
 	whoami = getpass.getuser()
@@ -12,8 +17,8 @@ except:
 	whoami = "viur"
 
 ap = argparse.ArgumentParser(
-		description="Setting up a clean ViUR project base.",
-		epilog="The script runs interactively if not command-line arguments are passed.")
+	description="Setting up a clean ViUR project base.",
+	epilog="The script runs interactively if not command-line arguments are passed.")
 
 ap.add_argument("-A", "--app_id", type=str, help="The application-id that should be replaced in the arbitrary places.")
 ap.add_argument("-a", "--author", type=str, default=whoami, help="The author's name that is placed in arbitrary places.")
@@ -24,11 +29,8 @@ app_id = args.app_id
 whoami = args.author
 
 if args.app_id is None:
-	prompt = "Enter Author Name (leave empty to default to %s): " % whoami 
-	try:
-		name = raw_input(prompt)
-	except NameError:
-		name = input(prompt)
+	prompt = f"Enter Author Name (leave empty to default to {whoami}): "
+	name = input(prompt)
 
 	if name:
 		whoami = name
@@ -38,22 +40,18 @@ if args.app_id is None:
 	if not app_id.endswith("-viur"):
 		app_id += "-viur"
 
-	prompt = "Enter application-id (leave empty to default to %s): " % app_id
-
-	try:
-		name = raw_input(prompt)
-	except NameError:
-		name = input(prompt)
+	prompt = f"Enter application-id (leave empty to default to {app_id}: "
+	name = input(prompt)
 
 	if name:
 		app_id = name
 
 time = time.time()
-timestamp = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+timestamp = datetime.datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S")
 
-workdir = os.getcwd()+"/deploy"
+workdir = os.getcwd() + "/deploy"
 file_list = ["viur-project.md", "local_run.sh"]
-replacements = {"{{app_id}}":app_id, "{{whoami}}":whoami, "{{timestamp}}":timestamp}
+replacements = {"{{app_id}}": app_id, "{{whoami}}": whoami, "{{timestamp}}": timestamp}
 
 # Build file list in which to search for placeholders to replace
 
@@ -73,7 +71,7 @@ for file_obj in file_list:
 			for src, target in replacements.items():
 				line = line.replace(src, target)
 			lines.append(line)
-	with open(file_obj, 'w') as outfile:
+	with open(file_obj, "w") as outfile:
 		for line in lines:
 			outfile.write(line)
 
@@ -81,12 +79,12 @@ for file_obj in file_list:
 
 if os.path.exists(".git"):
 	print("Downloading submodules")
-	subprocess.check_output('git submodule init', shell=True)
-	subprocess.check_output('git submodule update', shell=True)
-	subprocess.check_output('cd vi && git submodule init && git submodule update', shell=True)
+	subprocess.check_output("git submodule init", shell=True)
+	subprocess.check_output("git submodule update", shell=True)
+	subprocess.check_output("cd vi && git submodule init && git submodule update", shell=True)
 	print("Removing .git tether")
 	try:
-		subprocess.check_output('git remote rm origin', shell=True)
+		subprocess.check_output("git remote rm origin", shell=True)
 		print(".git remote tether removed")
 	except:
 		pass
@@ -98,26 +96,18 @@ else:
 zipname = "vi.zip"
 
 sys.stdout.write("Downloading latest build of vi...")
-urllib.urlretrieve("https://www.viur.is/package/download/vi/latest", zipname)
+urllib.request.urlretrieve("https://www.viur.is/package/download/vi/latest", zipname)
 print("Done downloading latest build of vi")
 
 sys.stdout.write("Extracting latest build of vi...")
 
 zip = zipfile.ZipFile(zipname, "r")
-zip.extractall('deploy/')
+zip.extractall("deploy/")
 zip.close()
 
 os.remove(zipname)
 
 print("Done extracting vi")
-
-# Rename viur project
-orig = os.path.join(workdir, "viur-project.py")
-newname = os.path.join(workdir, app_id+".py")
-os.rename(orig, newname)
-
-# Add newly renamed viur-project.py to local git
-subprocess.check_output('git add '+ newname, shell=True)
 
 # Create a README.md
 os.remove("README.md")  # this is needed because on windows os.rename will fail caused by existing dest!!!
