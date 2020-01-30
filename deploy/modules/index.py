dcbb3641eb0e7ebb9c7ee1571ef0cc3e7afb8088
Jan Max Meyer <jmm@phorward.de>
2020-01-30 15:35:14 +0100

    Little import fix (temporal)

--- deploy/modules/index.py
+++ deploy/modules/index.py
@@ -4,7 +4,8 @@ from viur.core.render.html import default as default_render
 
 # from google.appengine.api import urlfetch, app_identity
 
-import json, logging, datetime, httplib
+import json, logging, datetime
+#import httplib #fixme ViUR3 port
 
 
 class index(object):
