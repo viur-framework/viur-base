152b669105bac657a7248300c3e0b2c21e1d04db
Jan Max Meyer <jmm@phorward.de>
2020-01-31 18:07:42 +0100

    Updating submodules, provide default getAvailableRootNodes()

--- deploy/modules/file.py
+++ deploy/modules/file.py
@@ -2,4 +2,12 @@
 from viur.core.modules.file import File
 
 class file(File):
-	pass
+
+	def getAvailableRootNodes(self, *args, **kwargs):
+		if utils.getCurrentUser():
+			repo = self.ensureOwnModuleRootNode()
+
+			res = [{"name": "Files", "key": repo.key.id_or_name}]
+			return res
+
+		return []
