6be5e959c689745e783bc829598725187692dd2d
Jan Max Meyer <jmm@phorward.de>
2020-05-19 17:08:34 +0200

    Updating to latest ViUR 3 development versions

--- deploy/modules/file.py
+++ deploy/modules/file.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import datetime
 from viur.core.modules.file import File
-from viur.core import utils
+from viur.core import utils, db
+from viur.core.prototypes.uniformtree import TreeType
+
 
 class file(File):
 
@@ -8,7 +11,19 @@ class file(File):
 		if utils.getCurrentUser():
 			repo = self.ensureOwnModuleRootNode()
 
-			res = [{"name": "Files", "key": repo.key.id_or_name}]
+			res = [{"name": "Files", "key": repo.key}]
 			return res
 
 		return []
+
+	def ensureOwnModuleRootNode(self):
+		"""
+		Ensures, that general root-node for the current module exists.
+		If no root-node exists yet, it will be created.
+
+		:returns: The entity of the root-node.
+		:rtype: :class:`server.db.Entity`
+		"""
+		key = "rep_module_repo"
+		kindName = self.viewSkel(TreeType.Node).kindName
+		return db.GetOrInsert(db.Key(kindName, key), creationdate=datetime.datetime.now(), rootNode=1)
