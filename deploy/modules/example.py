572c5b86d3056f7be3f1910afbb0e3eb71f7ccab
Dennis Marin <dm@mausbrand.de>
2021-10-01 16:36:14 +0200

    Added example to derive images in various sizes and output it in jinja

--- deploy/modules/example.py
+++ deploy/modules/example.py
@@ -1,13 +1,8 @@
-from viur.core import exposed
 from viur.core.prototypes import List
 
 
 class Example(List):
+	listTemplate = "example_list"
 
 	def listFilter(self, query):
 		return query  # everyone can see everything!
-
-	@exposed
-	def index(self, *args, **kwargs):
-		# Return list function as default
-		return self.list(*args, **kwargs)
