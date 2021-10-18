cdd95412f1a84c7e5a7341d5e5ff894c7a727ecf
Jan Max Meyer <jm@mausbrand.de>
2021-10-19 00:46:20 +0200

    Added pipenv with app_server (#55)
    - Update to Python 3.9
    - local_run.sh allows either for app_server and dev_appserver.py

--- local_run.sh
+++ local_run.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-dev_appserver.py -A {{app_id}} $* deploy
+PROJECT_ID="{{app_id}} "
+
+which app_server >/dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	dev_appserver.py -A $PROJECT_ID $* deploy
+else
+	app_server -A $PROJECT_ID $* deploy
+fi
