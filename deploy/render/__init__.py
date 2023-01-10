e4ef48cf2ffe4e8f504ea575cbfc70e4deb3702a
Sven Eberth <mail@sveneberth.de>
2023-01-10 16:15:20 +0100

    Remove `isList` and `isDict` (#73)
    * Remove `isList` and `isDict`
    
    They are now replaced by the `list` and `dict` Jinja-tests in the viur-core (added in viur-framework/viur-core#532).
    
    * Change `|isList` and `|isDict` filters in skelview to `is list`/`is dict` tests

--- deploy/render/__init__.py
+++ deploy/render/__init__.py
@@ -1,9 +1,1 @@
 from viur.core.render import admin, html, json, vi, xml
-
-@html.utils.jinjaGlobalFilter
-def isList(render, val):
-	return isinstance(val, list)
-
-@html.utils.jinjaGlobalFilter
-def isDict(render, val):
-	return isinstance(val, dict)
