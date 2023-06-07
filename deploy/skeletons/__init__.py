65bb97ec8a11efe6207f695e6fd303c8c73d05c2
Jan Max Meyer <jmm@phorward.de>
2023-06-08 00:22:59 +0200

    Simplify skeletons/__init__.py
    It just now raises and exception and stops on error, it is so nice and useful...

--- deploy/skeletons/__init__.py
+++ deploy/skeletons/__init__.py
@@ -2,16 +2,12 @@
 # If any other importing logic is wanted, please switch to manual import calls in this file, and remove
 # the dynamic code provided below.
 
-import logging
 import os
 
 for skelModule in os.listdir(os.path.dirname(__file__)):
     if skelModule == "__init__.py" or not skelModule.endswith(".py"):
         continue
 
-    try:
-        __import__(skelModule[:-3], globals(), locals(), level=1)
-    except ImportError:
-        logging.error("Unable to import skeleton %s" % skelModule[:-3])
+    __import__(skelModule[:-3], globals(), locals(), level=1)
 
 del skelModule
