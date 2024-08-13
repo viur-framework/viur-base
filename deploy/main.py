df7d92af836f2e3b1d531657f1252866402abc53
Sven Eberth <mail@sveneberth.de>
2024-08-13 19:42:10 +0200

    chore: Update dependencies (#132)
    * viur-core
    * vi-admin
    * viur-cli

--- deploy/main.py
+++ deploy/main.py
@@ -91,6 +91,10 @@ conf.compatibility.remove("json.bone.structure.camelcasenames")  # render new ke
 # ViUR >= 3.5 compatibility feature disabling
 conf.compatibility.remove("json.bone.structure.inlists")  # disable structure rendering on list
 
+# ViUR >= 3.6 compatibility feature disabling
+# render old-style tuple-list in SelectBone's values structure
+conf.compatibility.remove("bone.select.structure.values.keytuple")
+
 # ------------------------------------------------------------------------------
 # File module
 #
