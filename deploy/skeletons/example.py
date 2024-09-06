2bf3b9779a6c222b568a8681123380296e95caf1
Jan Max Meyer <jmm@phorward.de>
2024-09-06 15:24:01 +0200

    More clean-up on viur-base
    - Added Google Login setup
    - Cleaned main.py
      - replaced SendInBlue by Mailjet example (commented)
      - added User-module section with roles and Google settings
    - Removed resources and deploy/vi_plugins
    - Fixed and cleaned Example-Module and skeleton
    - Moved most templates to deploy/html/default
    - Cleaned imports

--- deploy/skeletons/example.py
+++ deploy/skeletons/example.py
@@ -7,45 +7,10 @@ class ExampleSkel(Skeleton):
     # Defaults
     name = StringBone(
         descr="Name",
-        required=True
+        required=True,
     )
 
-    sortindex = NumericBone(
-        descr="Sort index",
-        indexed=True,
-        required=True
-    )
-
-    image = FileBone(
-        descr="Image",
-        derive=conf["derives"]
-    )
-
-    # SEO
-    seo_title = StringBone(
-        descr="SEO Title",
-        params={
-            "category": "SEO"
-        }
-    )
-
-    seo_description = StringBone(
-        descr="SEO Description",
-        params={
-            "category": "SEO"
-        }
-    )
-
-    seo_keywords = StringBone(
-        descr="SEO Keywords",
-        params={
-            "category": "SEO"
-        }
-    )
-
-    seo_image = FileBone(
-        descr="SEO Preview Image",
-        params={
-            "category": "SEO"
-        }
-    )
+    # image = FileBone(
+    #     descr="Image",
+    #     derive=conf.project.standard_derives,
+    # )
